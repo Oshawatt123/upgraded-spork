bea0207897ab679dccdecfe9e7c3b67d0cef488b
Oshawatt123 <lewis.arnold@hotmail.co.uk>
2019-10-18 15:32:08 +0100

    IMAGE RECOGNITION
    Image recognition works, but has trouble forgetting an image, and trouble anchoring a visualizer correctly. Will try with printed bits of paper and see if that makes a difference

---
+++ ARImage/ARImageProject/Assets/Scripts/AugmentedImageVisualizer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class AugmentedImageVisualizer : MonoBehaviour
+{
+
+    public AugmentedImage Image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Image == null || Image.TrackingState != TrackingState.Tracking)
+        {
+            return;
+        }
+
+        transform.localScale = new Vector3(Image.ExtentX, Image.ExtentZ, 1);
+
+    }
+}
---
+++ ARImage/ARImageProject/Assets/Scripts/VisualizerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+using System;
+
+public class VisualizerController : MonoBehaviour
+{
+    [SerializeField] private AugmentedImageVisualizer augmentedImageVisualizer;
+
+    private readonly Dictionary<int, AugmentedImageVisualizer> visualizers = new Dictionary<int, AugmentedImageVisualizer>();
+
+    private readonly List<AugmentedImage> images = new List<AugmentedImage>();
+
+    private void Update()
+    {
+        if(Session.Status != SessionStatus.Tracking)
+        {
+            return;
+        }
+
+        Session.GetTrackables(images, TrackableQueryFilter.Updated);
+        VisualizeTrackables();
+    }
+
+    private void VisualizeTrackables()
+    {
+        foreach (var image in images)
+        {
+            var visualizer = GetVisualizer(image);
+
+            // if tracking, but no visualizer
+            if(image.TrackingState == TrackingState.Tracking && visualizer == null)
+            {
+                AddVisluazier(image);
+            }
+            // else if tracking has stopped and still has a visualizer
+            else if (image.TrackingState == TrackingState.Stopped && visualizer != null)
+            {
+                RemoveVisualizer(image, visualizer);
+            }
+        }
+    }
+
+    private void AddVisluazier(AugmentedImage image)
+    {
+        // anchor is world position of our AR image so it can stay put in real world space when moving about
+        var anchor = image.CreateAnchor(image.CenterPose);
+
+        // create our visualized object
+        var visualizer = Instantiate(augmentedImageVisualizer, anchor.transform);
+        // set the image of the visualizer to the image we have tracked
+        visualizer.Image = image;
+        // add our visualizer to our list of visualizers
+        visualizers.Add(image.DatabaseIndex, visualizer);
+    }
+
+    private void RemoveVisualizer(AugmentedImage image, AugmentedImageVisualizer visualizer)
+    {
+        visualizers.Remove(image.DatabaseIndex);
+        Destroy(visualizer.gameObject);
+    }
+
+    private AugmentedImageVisualizer GetVisualizer(AugmentedImage image)
+    {
+        AugmentedImageVisualizer visualizer;
+        visualizers.TryGetValue(image.DatabaseIndex, out visualizer);
+        return visualizer;
+    }
+}
