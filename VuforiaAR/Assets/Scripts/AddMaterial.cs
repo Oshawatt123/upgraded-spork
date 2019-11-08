d2d531d0498c246909c0c85a4d044176fc1a0816
Oshawatt123 <lewis.arnold@hotmail.co.uk>
2019-11-08 14:48:15 +0000

    Vuforia Project

---
+++ VuforiaAR/Assets/Scripts/AddMaterial.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddMaterial : MonoBehaviour
+{
+
+    public Material newMaterial;
+    private Material[] mats;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddNewMaterial(Transform objectToHighlight)
+    {
+        mats = objectToHighlight.GetComponent<Renderer>().materials;
+
+        mats[0] = mats[0];
+        mats[1] = newMaterial;
+    }
+
+    public void SetDefaultMaterial(Transform objectToUnHighlight)
+    {
+        mats = objectToUnHighlight.GetComponent<Renderer>().materials;
+
+        mats[0] = mats[0];
+    }
+}
---
+++ VuforiaAR/Assets/Scripts/LookAtObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtObject : MonoBehaviour
+{
+
+    public AddMaterial addMaterialScript;
+    public Camera lookCamera;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 middleOfScreen = new Vector3(Screen.width / 2, Screen.height / 2);
+        Ray ray = lookCamera.ScreenPointToRay(middleOfScreen);
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            Transform objectHit = hit.transform;
+
+            addMaterialScript.AddNewMaterial(objectHit);
+        }
+    }
+}
