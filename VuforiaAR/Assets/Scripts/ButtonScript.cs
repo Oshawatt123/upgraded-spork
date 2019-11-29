319962e1605476adf0649d29601b35b5c3e71257
Oshawatt123 <lewis.arnold@hotmail.co.uk>
2019-11-29 12:45:05 +0000

    Main menu started

---
+++ VuforiaAR/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public void OpenCanvasGroup(CanvasGroup canvasGroup)
+    {
+        canvasGroup.alpha = 1f;
+        canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    public void CloseCanvasGroup(CanvasGroup canvasGroup)
+    {
+        canvasGroup.alpha = 0f;
+        canvasGroup.interactable = false;
+        canvasGroup.blocksRaycasts = false;
+    }
+}
